836e9b3b372f53dd3aa0a21aecc3d648689586b4
TeeNik <teenik3315@gmail.com>
2020-03-31 02:23:28 +0300

    text

---
+++ Solution/Header/Graphics/UI/Text.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <string>
+#include "Math/Math.h"
+
+class Texture;
+class Shader;
+class Font;
+
+class Text {
+
+public:
+	Text(const Font* font);
+	~Text();
+
+	void SetText(const std::string& text, const Vector3& color = Color::White, int pointSize = 30);
+	void SetText(const std::string& text);
+	void SetColor(const Vector3& color);
+	void SetPointSize(int size);
+
+	inline Texture* GetTexture() { return texture; }
+
+private:
+	std::string text;
+	int pointSize = 0;
+	Vector3 color;
+	Texture* texture = nullptr;
+	const Font* font = nullptr;
+};
---
+++ Solution/Source/Graphics/UI/Text.cpp
@@ -0,0 +1,51 @@
+#include "Graphics/UI/Text.hpp"
+#include "Engine/Engine.hpp"
+#include "Graphics/Renderer.hpp"
+#include "Graphics/UI/Font.hpp"
+#include "Graphics/Texture.hpp"
+
+Text::Text(const Font* f) : font(f)
+{
+}
+
+Text::~Text()
+{
+	if (texture != nullptr) {
+		texture->Unload();
+		delete texture;
+	}
+}
+
+void Text::SetText(const std::string& t, const Vector3& c, int s)
+{
+	if (text != t && color != c && pointSize != s) {
+		text = t;
+		color = c;
+		pointSize = s;
+		texture = font->RenderText(text, color, pointSize);
+	}
+}
+
+void Text::SetText(const std::string& t)
+{
+	if (text != t) {
+		text = t;
+		texture = font->RenderText(text, color, pointSize);
+	}
+}
+
+void Text::SetColor(const Vector3& c)
+{
+	if (color != c) {
+		color = c;
+		texture = font->RenderText(text, color, pointSize);
+	}
+}
+
+void Text::SetPointSize(int s)
+{
+	if (pointSize != s) {
+		pointSize = s;
+		texture = font->RenderText(text, color, pointSize);
+	}
+}
